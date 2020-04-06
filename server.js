fd1accefd377836177d1148009915c9f82fe7cd3
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-04-06 18:32:18 +0530

    add front end

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 var express = require("express");
+
+var app = express()
+
+app.use(express.static(__dirname))
+var server = app.listen(3000, () => {
+    console.log(`server listening on port`, server.address().port);
+})
