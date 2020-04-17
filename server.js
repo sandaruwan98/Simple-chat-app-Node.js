340aac9aacf1edb99c427fba419d2eb8b3a6f551
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-04-17 21:19:23 +0530

    Created a get messages service

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ var express = require("express");
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    { name: 'Lakshan', message: 'hi' },
+    { name: 'Sandaruwan', message: 'hello' }
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+
+
 var server = app.listen(3000, () => {
     console.log(`server listening on port`, server.address().port);
-})
+})  
