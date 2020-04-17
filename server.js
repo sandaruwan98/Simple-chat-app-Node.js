3d82cef91cee4a3932107dbcd040e9b3b26b70be
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-04-17 22:33:22 +0530

    Create a post messages service

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var bodyparser = require("body-parser");
 
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
 
 var messages = [
     { name: 'Lakshan', message: 'hi' },
@@ -11,6 +14,12 @@ var messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200);
+
 })
 
 
