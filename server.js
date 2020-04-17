33a3c1362bdbb7c7f8c74bf9407eb21659faa8f8
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-04-17 22:56:25 +0530

    socket.io added can be chat with two clients successfully msg history is not saving

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var bodyparser = require("body-parser");
 
 var app = express()
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
 
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
@@ -18,12 +20,16 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body)
+    io.emit("message", req.body)
     res.sendStatus(200);
 
 })
 
+io.on("connection", (socket) => {
+    console.log("a user connected");
+})
 
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log(`server listening on port`, server.address().port);
 })  
